95b7a4f0535c290cf7392f66b01957135f6449f8
ShravyaChandiri <schandiri@unomaha.edu>
2020-11-03 17:22:17 -0600

    CSV file generation from admin panel
    CSV file generation from admin panel and also fixed the events columns on admin panel as per the comment given by Dr.Royce during demo

--- events/admin.py
+++ events/admin.py
@@ -1,15 +1,107 @@
 from django.contrib import admin
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Location, Event, Enrollment
+from django.http import HttpResponse
+import csv
 
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+        formatted_field_names = []
+        for name in field_names:
+            if 'img' in name:
+                field_names.remove(name)
+                continue
+            else:
+                field_name = ""
+                for n in name.split('_'):
+                    n = n[0].upper() + n[1:]
+                    field_name += n + " "
+                formatted_field_names.append(field_name.rstrip())
+        response = HttpResponse(content_type='text/csv')
+        if str(meta) == "events.event":
+            meta = "Events_report"
+        elif str(meta) == "events.enrollment":
+            meta = "Enrollments_report"
+        else:
+            meta = "Locations_report"
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(formatted_field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected as CSV"
+
+
+class EnrollmentAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display =('event_name', 'event_date', 'victim_name', 'notes', 'is_important')
+    #adding the export functionality to this list make it visible in the actions dropdown on the admin panel.
+    actions = ["export_as_csv"]
+
+    def event_name(self, instance):
+        try:
+            return instance.event.event_name
+        except ObjectDoesNotExist:
+            return 'ERROR!!'
+
+    def event_date(self, instance):
+        try:
+            return instance.event.start_date_time.strftime("%A, %d %b %Y %H %M") + " to " + instance.event.end_date_time.strftime("%A, %d %b %Y %H %M")
+        except ObjectDoesNotExist:
+            return 'ERROR!!'
+
+    def victim_name(self, instance):
+        try:
+            return instance.victim.first_name + " " + instance.victim.last_name
+        except ObjectDoesNotExist:
+            return 'ERROR!!'
+
+    def action_requried(self, instance):
+        try:
+            return "Yes" if instance.is_important else "No"
+        except ObjectDoesNotExist:
+            return 'ERROR!!'
 
 class EnrollmentInline(admin.TabularInline):
     model = Enrollment
 
-class ActivityAdmin(admin.ModelAdmin):
+class ActivityAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = ("event_name", "event_date")
+    actions = ["export_as_csv"]
+
+    def event_date(self, instance):
+        try:
+            return instance.start_date_time.strftime("%A, %d %b %Y %H %M") + " to " + instance.end_date_time.strftime("%A, %d %b %Y %H %M")
+        except ObjectDoesNotExist:
+            return 'ERROR!!'
+
     inlines = [
         EnrollmentInline,
     ]
 
-admin.site.register(Location)
+class LocationAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = ("location_name", "full_address")
+    actions = ["export_as_csv"]
+
+    def location_name(self, instance):
+        try:
+            return instance.name
+        except ObjectDoesNotExist:
+            return 'ERROR!!'
+
+    def full_address(self, instance):
+        try:
+            return instance.address + ", " + instance.city + ", " + instance.state + ", " + str(instance.zip)
+        except ObjectDoesNotExist:
+            return 'ERROR!!'
+
+admin.site.register(Location, LocationAdmin)
 admin.site.register(Event, ActivityAdmin)
-admin.site.register(Enrollment)
+admin.site.register(Enrollment, EnrollmentAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,58 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.core.exceptions import ObjectDoesNotExist
-
 from .models import Volunteer, Staff, Victim
 
 
-class VolunteerAdmin(admin.ModelAdmin):
+from django.http import HttpResponse
+import csv
+
+class VictimExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        # Getting only the fields required
+        victims = Victim.objects.all().only("id", "first_name", "last_name", "email", "mobile_num", \
+                                               "street_address", "city", "state", "zip", "disease_type",
+                                               "notes")
+        # Specify the header field names
+        field_names = ['Victim ID', 'Victim Name', 'Email', 'Phone', 'Address', 'City', 'State', 'ZIP',\
+                                 'Disease type', 'Notes']
+        response = HttpResponse(content_type='text/csv')
+        # Specify the filename
+        response['Content-Disposition'] = 'attachment; filename=Victims_report.csv'
+        writer = csv.writer(response)
+        writer.writerow(field_names)
+        for victim in victims:
+            writer.writerow([victim.id, victim.first_name + " " + victim.last_name, victim.email, victim.mobile_num, \
+                             victim.street_address, victim.city, victim.state, victim.zip, victim.disease_type,\
+                             victim.notes])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected as CSV"
+
+class VolunteerExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        volunteers = Volunteer.objects.all().only("id", "first_name", "last_name", "email", "mobile_num", "work_phone",\
+            "street_address", "city", "state", "zip", "time_spent", "specialized_in", "comments")
+        formatted_field_names = ['Volunteer ID', 'Volunteer Name', 'Email', 'Phone',\
+                                 'Work Phone', 'Address', 'City', 'State', 'ZIP', 'Number of working hours per week',\
+                                 'Speciality', 'Additional Information']
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=Volunteers_report.csv'
+        writer = csv.writer(response)
+        writer.writerow(formatted_field_names)
+        for volunteer in volunteers:
+            writer.writerow([volunteer.id, volunteer.first_name + " " + volunteer.last_name, volunteer.email, volunteer.mobile_num, volunteer.work_phone,\
+                             volunteer.street_address, volunteer.city, volunteer.state, volunteer.zip, volunteer.time_spent,\
+                             volunteer.specialized_in.first(), volunteer.comments])
+
+        return response
+    export_as_csv.short_description = "Export Selected as CSV"
+
+class VolunteerAdmin(admin.ModelAdmin, VolunteerExportCsvMixin):
     list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'work_phone')
+    actions = ["export_as_csv"]
 
     def username(self, instance):  # name of the method should be same as the field given in `list_display`
         try:
@@ -41,6 +87,7 @@ class VolunteerAdmin(admin.ModelAdmin):
 
 class StaffAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'work_phone')
+    actions = ["export_as_csv"]
 
     def username(self, instance):  # name of the method should be same as the field given in `list_display`
         try:
@@ -72,8 +119,9 @@ class StaffAdmin(admin.ModelAdmin):
         except ObjectDoesNotExist:
             return 'ERROR!!'
 
-class VictimAdmin(admin.ModelAdmin):
+class VictimAdmin(admin.ModelAdmin, VictimExportCsvMixin):
     list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'disease_type')
+    actions = ["export_as_csv"]
 
     def username(self, instance):  # name of the method should be same as the field given in `list_display`
         try:
