d3a71c5d75cdf4d8e691d1d854f5d59408dee925
ShravyaChandiri <schandiri@unomaha.edu>
2020-11-17 18:52:24 -0600

    Corrected the display of nav-items (Moved them to right)
    Corrected the display of nav-items (Moved them to right)

--- users/admin.py
+++ users/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.core.exceptions import ObjectDoesNotExist
 from .models import *
-
-
 from django.http import HttpResponse
 import csv
 
